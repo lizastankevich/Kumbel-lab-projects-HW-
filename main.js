535865f65c632a76a295c5c226b3011abaf52c33
lizastankevich <S23070315@narxoz.kz>
2025-01-21 22:03:41 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
-function convertDegree(celciusDegree){ 
-  let forengeitDegree = (celciusDegree*9)/5+32; 
-    console.log(celciusDegree ,"\u00b0C - " + forengeitDegree.toFixed(0),"\u00b0F");
-}
+//My global scope
+const globalVar = "I am the world itself";
 
-convertDegree(30);
-convertDegree(50);
-convertDegree(100);
-
-function convertTemperature(forengeitDegree){ 
-  let celciusDegree = (forengeitDegree-32)*5/9; 
-    console.log(forengeitDegree,"\u00b0F - " + celciusDegree.toFixed(0),"\u00b0C");
+//Function scope or local
+function scopes() {
+  const localVar = "It is Kazachstan";
+  console.log(globalVar,"global var inside");
+  console.log(localVar,"local var inside");
+  //block scope
+  {
+    const blockVar = "The capital of Kazachstan is Astana";
+    console.log(blockVar,"block var inside");
+  }
+  // console.log(blockVar, "- block var outside block"); //will cause an error because it is ou0tside the block scope
 }
-// use to fixed
-convertTemperature(150);
-convertTemperature(200);
-convertTemperature(400);
+//2 function scope 
+  function scopes2() {
+    const localVar2 = "It is Russia";
+    console.log(localVar2,"local var inside ");
+    {
+      //block scope of second function
+      const blockVar2 = "The capital of Russia is Moscow";
+      console.log(blockVar2,"block var inside");
+    }
+    // console.log(blockVar2, "- block var outside block"); //will cause an error because it is outside the block scope
+}
+scopes()
+console.log(globalVar,"global var outside");//work in any part of the code because it is global
+//console.log(localVar,"local var outside");//will cause error because it is outside the function scope
+scopes2()
+//console.log(localVar2,"local var outside");//LocalVar2 it not defined  because it is outside the function scope
