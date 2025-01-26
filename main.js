3754deee0ce377c242a776b2b48c746ff86d4a12
lizastankevich <S23070315@narxoz.kz>
2025-01-26 23:10:23 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+function nameCondition(name){
+     if(name === "Jasmin") {
+   console.log("Сегодня твой счастливый день! получи свой приз");
+     } else if(name === "Aidana") {
+   console.log("Сегодня и твой счастливый день! получи свою скидку");
+   } else if(name === "Saya") {console.log("Добро пожаловать на страничку моей домашки") } else {
+   console.log("Добро пожаловать "+ name);
+   }
+}
+nameCondition("Jasmin");
+nameCondition("Aidana");
+nameCondition("Liza");
+nameCondition("Saya");
