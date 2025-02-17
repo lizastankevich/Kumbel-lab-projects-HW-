b6a3c5658077899018dc446a2f06b96df8691912
lizastankevich <S23070315@narxoz.kz>
2025-02-17 13:47:10 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+const Sentence = "The quick brown fox jumps over the lazy dog.If the dog barked/was it really lazy";
+const searchTerm = 'dog';
+console.log(Sentence.indexOf(searchTerm));
+console.log(Sentence.lastIndexOf(searchTerm));
